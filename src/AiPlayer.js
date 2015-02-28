d083d021b2bb9bd948cc9320cf194fbaec4e0eef
Matt Fender <mhfender@Matts-MacBook-Air.local>
2015-02-28 12:44:34 -0600

    Changed AiPlayer class to JS syntax standards

--- src/AiPlayer.js
+++ src/AiPlayer.js
@@ -1,16 +1,35 @@
-var ticTacToe = ticTacToe || {}
+/**
+ * @fileOverview AiPlayer module definition
+ */
 
-;(function (app) {
-  app.AiPlayer = function (options)
-  {
-      if (!options) {
-        options = {}
-      }
-      this.name = options.name || "AI Player";
-  }
+var ticTacToe = ticTacToe || {};
 
-  app.AiPlayer.prototype.getName = function()
-  {
-      return this.name;
-  }
+(function (app) {
+
+  /**
+   * A class that plays TicTacToe without user input
+   *
+   * @class AiPlayer
+   * @constructor
+   * @param {Object} options A set of options chosen by the instantiator of the class
+   */
+  app.AiPlayer = function (options) {
+    if (!options) {
+      this.options = {};
+    } else {
+      this.options = options;
+    }
+    this.name = options.name || "AI Player";
+  };
+
+  /**
+   * Returns the name of the AiPlayer instantiation
+   *
+   * @method getName
+   * @returns this.name The name of the AiPlayer instantiation
+   */
+  app.AiPlayer.prototype.getName = function() {
+    return this.name;
+  };
+
 })(ticTacToe);
