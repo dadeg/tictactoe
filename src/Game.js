fa08c40f2965f1baaf8c615f126bafd072b75bdd
Matt Fender <mhfender@gmail.com>
2015-02-28 15:21:39 -0600

    Added the methods necessary to run the game

--- src/Game.js
+++ src/Game.js
@@ -34,9 +34,18 @@ var Game = function(player1, player2) {
    *
    * @property gameBoard
    * @type {Array}
-   * @default undefined
+   * @default [0,0,0,0,0,0,0,0,0]
    */
-  this.gameBoard = [];
+  this.gameBoard = [0,0,0,0,0,0,0,0,0];
+
+  /**
+   * Tracks whether the game is won by either player
+   *
+   * @property gameIsNotWon
+   * @type {bool}
+   * @default true
+   */
+  this.gameIsNotWon = true;
 
   this.init();
 };
@@ -48,8 +57,8 @@ var Game = function(player1, player2) {
  * @returns Game
  */
 Game.prototype.init = function() {
-
-  this.assignSides();
+  this.assignSides()
+      .runGame(this.player1);
 
   return this;
 };
@@ -59,10 +68,75 @@ Game.prototype.init = function() {
  *
  * @method assignSides
  * @returns Game
+ * @chainable
  */
 Game.prototype.assignSides = function() {
   this.player1.side = 1;
   this.player2.side = 2;
+
+  return this;
+};
+
+/**
+ * Recursively runs getMove methods on the players until
+ * there is a winner
+ *
+ * @method runGame
+ * @param {Object} currentPlayer The current player of the game
+ */
+Game.prototype.runGame = function(currentPlayer) {
+  var move;
+  var nextPlayer;
+
+  if (currentPlayer === this.player1) {
+    nextPlayer = this.player2;
+  } else {
+    nextPlayer = this.player1;
+  }
+
+  if (this.gameIsNotWon) {
+    move = currentPlayer.getMove(this.gameBoard, currentPlayer.side);
+    this.updateGameBoard(move, currentPlayer);
+    this.checkForWin();
+    this.runGame(nextPlayer);
+  } else {
+    this.publishWin();
+  }
+};
+
+/**
+ * Updates the game board with the current player move
+ *
+ * @method updateGameBoard
+ * @param {int} move The index of the game board array where the move is made
+ * @param {Object} currentPlayer The current player of the game
+ * @chainable
+ */
+Game.prototype.updateGameBoard = function(move, currentPlayer) {
+  this.gameBoard[move] = currentPlayer.side;
+
+  return this;
+};
+
+/**
+ * Checks the game board to see if the current player's move wins the game
+ * or draws the game
+ *
+ * @method checkForWin
+ * @chainable
+ */
+Game.prototype.checkForWin = function() {
+
+  return this;
+};
+
+/**
+ * Publishes to a PubSub object with winner information for use by other modules
+ *
+ * @method publishWin
+ * @chainable
+ */
+Game.prototype.publishWin = function() {
 
   return this;
 };
