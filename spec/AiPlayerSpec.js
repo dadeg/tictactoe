b289f67dedb4622f1b0dd3959ba899e11a9f35c5
Dan DeGreef <dan.degreef@offers.com>
2015-03-02 08:21:28 -0600

    added rest of AI

--- spec/AiPlayerSpec.js
+++ spec/AiPlayerSpec.js
@@ -58,5 +58,105 @@ describe("AiPlayer", function() {
     expect(aiPlayer.getMove(board, side)).toEqual(8);
   });
 
+  it("can block an opponent from winning in vertical positions", function() {
+    board = [2,1,0,0,1,2,0,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(7);
+  });
+
+  it("can block an opponent from winning in horizontal positions", function() {
+    board = [2,0,0,2,0,0,1,1,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(8);
+  });
+
+  it("can block an opponent from winning in diagonal positions", function() {
+    board = [1,2,0,2,1,0,0,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(8);
+  });
+
+  it("can choose a diagonal move to make two in a row", function() {
+    board = [2,0,0,1,0,0,0,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(8);
+    board = [0,1,2,0,0,0,0,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(6);
+    board = [0,1,0,0,0,0,0,0,2];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(0);
+    board = [0,1,0,0,0,0,2,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(2);
+    board = [0,1,0,0,2,0,0,0,0];
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(8);
+  });
+
+  it("can block a diagonal move to make two in a row by opponent", function() {
+    board = [2,1,0,0,0,0,0,2,0];
+    side = 1;
+    expect(aiPlayer.getMove(board, side)).toEqual(8);
+  });
+
+  it("can choose a vertical move to make two in a row", function() {
+    board = [1,0,0,0,0,0,0,0,2]; // 2 blocks the preferable diagonal
+    side = 1;
+    expect(aiPlayer.getMove(board, side)).toEqual(6);
+  });
+
+  it("can choose a horizontal move to make two in a row", function() {
+    board = [1,0,0,2,0,0,0,0,2]; // 2 blocks the preferable diagonal and vertical
+    side = 1;
+    expect(aiPlayer.getMove(board, side)).toEqual(2);
+  });
+
+  it("can choose a vertical move to block an opponent from making two in a row", function() {
+    board = [1,0,0,0,0,1,0,1,2]; // 2 blocks the preferable diagonal
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(6);
+  });
+
+  it("can choose a horizontal move to block an opponent from making two in a row", function() {
+    board = [1,0,0,1,1,1,2,2,2]; // isolating the horizontal row alone.
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(2);
+  });
+
+  it("can play a perfect game as player 1", function() {
+    board = [0,0,0,0,0,0,0,0,0];
+    side = 1;
+    expect(aiPlayer.getMove(board, side)).toEqual(0); // p1 plays corner.
+    board = [1,0,0,0,2,0,0,0,0]; // p2 plays middle.
+    expect(aiPlayer.getMove(board, side)).toEqual(8); // p1 plays opposite corner.
+    board = [1,0,0,0,2,2,0,0,1]; // p2 plays side but not corner.
+    expect(aiPlayer.getMove(board, side)).toEqual(3); // p1 blocks p2 with other corner.
+    board = [1,0,0,1,2,2,2,0,1]; // p2 blocks p1.
+    expect(aiPlayer.getMove(board, side)).toEqual(2); // p1 blocks p2.
+    board = [1,2,1,1,2,2,2,0,1]; // p2 blocks p1.
+    expect(aiPlayer.getMove(board, side)).toEqual(7); // p1 blocks p2. draw.
+  });
+
+  it("can play a perfect game as player 2", function() {
+    board = [1,0,0,0,0,0,0,0,0]; // p1 plays corner
+    side = 2;
+    expect(aiPlayer.getMove(board, side)).toEqual(4); // p2 plays middle.
+    board = [1,0,0,0,2,0,0,0,1]; // p1 plays opposite corner.
+    expect(aiPlayer.getMove(board, side)).toEqual(1); // p2 plays side but not corner.
+    board = [1,2,0,0,2,0,0,1,1]; // p1 plays side but not corner to block.
+    expect(aiPlayer.getMove(board, side)).toEqual(6); // p2 blocks p1 with other corner.
+    board = [1,2,1,0,2,0,2,1,1]; // p1 blocks p2.
+    expect(aiPlayer.getMove(board, side)).toEqual(5); // p2 blocks p1.
+    board = [1,2,1,0,2,2,2,1,1]; // p1 blocks p2.
+    expect(aiPlayer.getMove(board, side)).toEqual(3); // p2 blocks p1. draw.
+  });
+
+  it("can play itself", function() {
+
+  });
+
+
+
 
 });
